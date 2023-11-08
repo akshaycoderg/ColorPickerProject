81ee159b231bd02e5599c444d3ad2fece3beb251
Akshay Thakur <109449229+akshaycoderg@users.noreply.github.com>
2023-11-08 08:37:34 +0530

    Initial Commit

---
+++ colorPickerProject.js
@@ -0,0 +1,24 @@
+let colorCode = document.getElementById("selectedColorHexCode")
+let bg_color = document.getElementById("colorPickerContainer")
+
+
+function color1() {
+    bg_color.style.background = "#e0e0e0"
+    colorCode.textContent = "#e0e0e0"
+}
+
+function color2() {
+    bg_color.style.background = "#6fcf97"
+    colorCode.textContent = "#6fcf97"
+}
+
+function color3() {
+    bg_color.style.background = "#56ccf2"
+    colorCode.textContent = "#56ccf2"
+    bg_color.style.backgroundszi
+}
+
+function color4() {
+    bg_color.style.background = "#bb6bd9"
+    colorCode.textContent = "#bb6bd9"
+}
